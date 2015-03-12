1f56c7e92004484ff1393d1f0af5d6f6255b3f4e
Matt Johnson <matt.johnson@microsoft.com>
2015-03-11 21:21:02 -0700

    Implement GetDatePart
    Borrowed from System.DateTime implementation

--- System.DateAndTime/Date.cs
+++ System.DateAndTime/Date.cs
@@ -15,9 +15,33 @@ namespace System
         public static readonly Date MinValue = new Date(MinDayNumber);
         public static readonly Date MaxValue = new Date(MaxDayNumber);
 
+        // Number of days in a non-leap year
+        private const int DaysPerYear = 365;
+        // Number of days in 4 years
+        private const int DaysPer4Years = DaysPerYear * 4 + 1;       // 1461
+        // Number of days in 100 years
+        private const int DaysPer100Years = DaysPer4Years * 25 - 1;  // 36524
+        // Number of days in 400 years
+        private const int DaysPer400Years = DaysPer100Years * 4 + 1; // 146097
+
+        // Number of days from 1/1/0001 to 12/31/1600
+        private const int DaysTo1601 = DaysPer400Years * 4;          // 584388
+        // Number of days from 1/1/0001 to 12/30/1899
+        private const int DaysTo1899 = DaysPer400Years * 4 + DaysPer100Years * 3 - 367;
+        // Number of days from 1/1/0001 to 12/31/1969
+        internal const int DaysTo1970 = DaysPer400Years * 4 + DaysPer100Years * 3 + DaysPer4Years * 17 + DaysPerYear; // 719,162
+        // Number of days from 1/1/0001 to 12/31/9999
+        private const int DaysTo10000 = DaysPer400Years * 25 - 366;  // 3652059
+
         // The following arrays contain the starting day-of-year number of each month, for regular and leap years
         private static readonly int[] DaysToMonth365 = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
         private static readonly int[] DaysToMonth366 = { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 };
+
+        // internal enum
+        private const int DatePartYear = 0;
+        private const int DatePartDayOfYear = 1;
+        private const int DatePartMonth = 2;
+        private const int DatePartDay = 3;
 
         // Number of whole days since 0001-01-01 (which is day 0)
         private readonly int _dayNumber;
@@ -47,32 +71,62 @@ namespace System
 
         public int Year
         {
-            get { return ToDateTimeAtMidnight().Year; }
+            get
+            {
+                Contract.Ensures(Contract.Result<int>() >= 1);
+                Contract.Ensures(Contract.Result<int>() <= 9999);
+                return GetDatePart(DatePartYear);
+            }
         }
 
         public int Month
         {
-            get { return ToDateTimeAtMidnight().Month; }
+            get
+            {
+                Contract.Ensures(Contract.Result<int>() >= 1);
+                Contract.Ensures(Contract.Result<int>() <= 12);
+                return GetDatePart(DatePartMonth);
+            }
         }
 
         public int Day
         {
-            get { return ToDateTimeAtMidnight().Day; }
+            get
+            {
+                Contract.Ensures(Contract.Result<int>() >= 1);
+                Contract.Ensures(Contract.Result<int>() <= 31);
+                return GetDatePart(DatePartDay);
+            }
         }
 
         public int DayOfYear
         {
-            get { return ToDateTimeAtMidnight().DayOfYear; }
+            get
+            {
+                Contract.Ensures(Contract.Result<int>() >= 1);
+                Contract.Ensures(Contract.Result<int>() <= 366);
+                return GetDatePart(DatePartDayOfYear);
+            }
         }
 
         public DayOfWeek DayOfWeek
         {
-            get { return ToDateTimeAtMidnight().DayOfWeek; }
+            get
+            {
+                Contract.Ensures(Contract.Result<DayOfWeek>() >= DayOfWeek.Sunday);
+                Contract.Ensures(Contract.Result<DayOfWeek>() <= DayOfWeek.Saturday);
+                return (DayOfWeek)((_dayNumber + 1) % 7);
+            }
         }
 
         public int DayNumber
         {
-            get { return _dayNumber; }
+            get
+            {
+                Contract.Ensures(Contract.Result<int>() >= MinDayNumber);
+                Contract.Ensures(Contract.Result<int>() <= MaxDayNumber);
+                return _dayNumber;
+            }
         }
 
         public DateTime At(TimeOfDay time)
@@ -338,6 +392,71 @@ namespace System
                 }
             }
             throw new ArgumentOutOfRangeException();
+        }
+
+        // Returns a given date part of this DateTime. This method is used
+        // to compute the year, day-of-year, month, or day part.
+        private int GetDatePart(int part)
+        {
+            // n = number of days since 1/1/0001
+            int n = _dayNumber;
+
+            // y400 = number of whole 400-year periods since 1/1/0001
+            int y400 = n / DaysPer400Years;
+
+            // n = day number within 400-year period
+            n -= y400 * DaysPer400Years;
+
+            // y100 = number of whole 100-year periods within 400-year period
+            int y100 = n / DaysPer100Years;
+
+            // Last 100-year period has an extra day, so decrement result if 4
+            if (y100 == 4) y100 = 3;
+
+            // n = day number within 100-year period
+            n -= y100 * DaysPer100Years;
+
+            // y4 = number of whole 4-year periods within 100-year period
+            int y4 = n / DaysPer4Years;
+
+            // n = day number within 4-year period
+            n -= y4 * DaysPer4Years;
+
+            // y1 = number of whole years within 4-year period
+            int y1 = n / DaysPerYear;
+
+            // Last year has an extra day, so decrement result if 4
+            if (y1 == 4) y1 = 3;
+
+            // If year was requested, compute and return it
+            if (part == DatePartYear)
+            {
+                return y400 * 400 + y100 * 100 + y4 * 4 + y1 + 1;
+            }
+
+            // n = day number within year
+            n -= y1 * DaysPerYear;
+
+            // If day-of-year was requested, return it
+            if (part == DatePartDayOfYear) return n + 1;
+
+            // Leap year calculation looks different from IsLeapYear since y1, y4,
+            // and y100 are relative to year 1, not year 0
+            bool leapYear = y1 == 3 && (y4 != 24 || y100 == 3);
+            int[] days = leapYear ? DaysToMonth366 : DaysToMonth365;
+
+            // All months have less than 32 days, so n >> 5 is a good conservative
+            // estimate for the month
+            int m = n >> 5 + 1;
+
+            // m = 1-based month number
+            while (n >= days[m]) m++;
+
+            // If month was requested, return it
+            if (part == DatePartMonth) return m;
+
+            // Return 1-based day-of-month
+            return n - days[m - 1] + 1;
         }
     }
 }
