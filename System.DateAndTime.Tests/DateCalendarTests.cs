783c5dc135a819d32e4da4c6f6dfc0c7fafb095a
Matt Johnson <matt.johnson@microsoft.com>
2015-04-21 21:48:29 -0700

    Add test for Date in Calendar

--- System.DateAndTime.Tests/DateCalendarTests.cs
+++ System.DateAndTime.Tests/DateCalendarTests.cs
@@ -12,5 +12,20 @@ namespace System.DateAndTime.Tests
             var expected = new Date(2015, 1, 1);
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void CanUseDateInCalendar()
+        {
+            var dt = new Date(2015, 1, 1);
+
+            var calendar = new UmAlQuraCalendar();
+            var year = calendar.GetYear(dt);
+            var month = calendar.GetMonth(dt);
+            var day = calendar.GetDayOfMonth(dt);
+            
+            Assert.Equal(1436, year);
+            Assert.Equal(3, month);
+            Assert.Equal(10, day);
+        }
     }
 }
