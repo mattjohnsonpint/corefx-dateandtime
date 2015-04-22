d9e212d9f0740212b8f4269147e7285f6f58e90b
Matt Johnson <matt.johnson@microsoft.com>
2015-04-21 20:50:52 -0700

    Add extension methods for TimeZoneInfo

---
+++ System.DateAndTime/TimeZoneInfoEx.cs
@@ -0,0 +1,25 @@
+﻿namespace System
+{
+    public static class TimeZoneInfoEx
+    {
+        public static DateTimeOffset GetCurrentDateTimeOffset(this TimeZoneInfo timeZoneInfo)
+        {
+            return DateTimeOffsetEx.NowInTimeZone(timeZoneInfo);
+        }
+
+        public static DateTime GetCurrentDateTime(this TimeZoneInfo timeZoneInfo)
+        {
+            return DateTimeEx.NowInTimeZone(timeZoneInfo);
+        }
+        
+        public static Date GetCurrentDate(this TimeZoneInfo timeZoneInfo)
+        {
+            return Date.TodayInTimeZone(timeZoneInfo);
+        }
+
+        public static TimeOfDay GetCurrentTime(this TimeZoneInfo timeZoneInfo)
+        {
+            return TimeOfDay.NowInTimeZone(timeZoneInfo);
+        }
+    }
+}
