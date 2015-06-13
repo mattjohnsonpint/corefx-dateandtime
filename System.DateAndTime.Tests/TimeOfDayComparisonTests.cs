c5e2993d2077ed955b139a834ba7bf99030c79bc
Matt Johnson <matt.johnson@microsoft.com>
2015-06-13 15:59:48 -0700

    Add more time comparison tests

--- System.DateAndTime.Tests/TimeOfDayComparisonTests.cs
+++ System.DateAndTime.Tests/TimeOfDayComparisonTests.cs
@@ -5,6 +5,316 @@ namespace System.DateAndTime.Tests
     public class TimeOfDayComparisonTests
     {
         [Fact]
+        public void CanCompareTimes_Static_Before()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var c = TimeOfDay.Compare(t1, t2);
+
+            Assert.Equal(-1, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Static_Equal()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var c = TimeOfDay.Compare(t1, t2);
+
+            Assert.Equal(0, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Static_After()
+        {
+            var t1 = new TimeOfDay(0, 1);
+            var t2 = new TimeOfDay(0, 0);
+
+            var c = TimeOfDay.Compare(t1, t2);
+
+            Assert.Equal(1, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Instance_Before()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var c = t1.CompareTo(t2);
+
+            Assert.Equal(-1, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Instance_Equal()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var c = t1.CompareTo(t2);
+
+            Assert.Equal(0, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Instance_After()
+        {
+            var t1 = new TimeOfDay(0, 1);
+            var t2 = new TimeOfDay(0, 0);
+
+            var c = t1.CompareTo(t2);
+
+            Assert.Equal(1, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Object_Before()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var c = t1.CompareTo((object)t2);
+
+            Assert.Equal(-1, c);
+        }
+
+        [Fact]
+        public void CanCompareTimes_Object_Equal()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var c = t1.CompareTo((object)t2);
+
+            Assert.Equal(0, c);
+        }
+
+        [Fact]
+        public void CanEquateTimes_Static_Equal()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = TimeOfDay.Equals(t1, t2);
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanEquateTimes_Static_NotEqual()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = TimeOfDay.Equals(t1, t2);
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanEquateTimes_Instance_Equal()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1.Equals(t2);
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanEquateTimes_Instance_NotEqual()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1.Equals(t2);
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanEquateTimes_Object_Equal()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1.Equals((object)t2);
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanEquateTimes_Object_NotEqual()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1.Equals((object)t2);
+
+            Assert.False(b);
+        }
+
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_LT1()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1 < t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_LT2()
+        {
+            var t1 = new TimeOfDay(0, 1);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 < t2;
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_LTE1()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1 <= t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_LTE2()
+        {
+            var t1 = new TimeOfDay(0, 1);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 <= t2;
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_LTE3()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 <= t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_EQ1()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 == t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_EQ2()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1 == t2;
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_NE1()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1 != t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_NE2()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 != t2;
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_GTE1()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 >= t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_GTE2()
+        {
+            var t1 = new TimeOfDay(0, 1);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 >= t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_GTE3()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1 >= t2;
+
+            Assert.False(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_GT1()
+        {
+            var t1 = new TimeOfDay(0, 1);
+            var t2 = new TimeOfDay(0, 0);
+
+            var b = t1 > t2;
+
+            Assert.True(b);
+        }
+
+        [Fact]
+        public void CanCompareTimes_UsingOperator_GT2()
+        {
+            var t1 = new TimeOfDay(0, 0);
+            var t2 = new TimeOfDay(0, 1);
+
+            var b = t1 > t2;
+
+            Assert.False(b);
+        }
+
+
+        [Fact]
         public void CanDetermineTimeInBetween_NormalInside()
         {
             TimeOfDay startTime = new TimeOfDay(10, 0);
