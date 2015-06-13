b3dd248282bbe42c01816195aa54ceecda409aa2
Matt Johnson <matt.johnson@microsoft.com>
2015-06-13 11:23:24 -0700

    Add more Date construction tests

--- System.DateAndTime.Tests/DateConstructionTests.cs
+++ System.DateAndTime.Tests/DateConstructionTests.cs
@@ -31,10 +31,99 @@ namespace System.DateAndTime.Tests
         }
 
         [Fact]
-        public void CanConstructDateFromParts()
+        public void CanConstructDateFromParts_Min()
+        {
+            Date date = new Date(1, 1, 1);
+            Assert.Equal(0, date.DayNumber);
+        }
+
+        [Fact]
+        public void CanConstructDateFromParts_Max()
         {
             Date date = new Date(9999, 12, 31);
             Assert.Equal(3652058, date.DayNumber);
+        }
+
+        [Fact]
+        public void CanConstructDateFromParts_MaxDay_31()
+        {
+            Date date = new Date(2000, 1, 31);
+            Assert.Equal(730149, date.DayNumber);
+        }
+
+        [Fact]
+        public void CanConstructDateFromParts_MaxDay_30()
+        {
+            Date date = new Date(2000, 4, 30);
+            Assert.Equal(730239, date.DayNumber);
+        }
+
+        [Fact]
+        public void CanConstructDateFromParts_MaxDay_29()
+        {
+            Date date = new Date(2000, 2, 29);
+            Assert.Equal(730178, date.DayNumber);
+        }
+
+        [Fact]
+        public void CanConstructDateFromParts_MaxDay_28()
+        {
+            Date date = new Date(2001, 2, 28);
+            Assert.Equal(730543, date.DayNumber);
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_YearTooSmall()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(-1, 1, 1));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_YearTooLarge()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(10000, 1, 1));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_MonthTooSmall()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2000, -1, 1));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_MonthTooLarge()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2000, 13, 1));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_DayTooSmall()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2000, 1, -1));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_DayTooLarge_31()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2000, 1, 32));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_DayTooLarge_30()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2000, 4, 31));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_DayTooLarge_29()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2000, 2, 30));
+        }
+
+        [Fact]
+        public void CannotConstructDateFromParts_DayTooLarge_28()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Date(2001, 2, 29));
         }
 
         [Fact]
